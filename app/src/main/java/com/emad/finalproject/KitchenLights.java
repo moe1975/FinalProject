ea897d18ad4852eba3c1e4881f4b3278a6aa6521
efawaz <abde0064@algonquinlive.com>
2017-03-30 04:36:51 -0400

    beta6

--- app/src/main/java/com/emad/finalproject/KitchenLights.java
+++ app/src/main/java/com/emad/finalproject/KitchenLights.java
@@ -3,6 +3,8 @@ package com.emad.finalproject;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.moe.finalproject.R;
+
 public class KitchenLights extends AppCompatActivity {
 
     @Override
